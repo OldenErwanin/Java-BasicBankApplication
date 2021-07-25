ef6b6394bb0e40409e43c15ef004615df575d970
OldenErwanin <51171007+OldenErwanin@users.noreply.github.com>
2021-07-25 21:23:37 +0200

    Update FirstBank.java

--- FirstBank.java
+++ FirstBank.java
@@ -1,6 +1,6 @@
 public class FirstBank {
     public static void main(String[] args) {
-        Account adam = new Account("Adam Tyihak", "A00001");
+        Account adam = new Account("Adam", "A00001");
         adam.showMenu();
     }
 }
